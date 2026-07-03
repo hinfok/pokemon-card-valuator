5fb3a7aca51e1539c95a8c76b24534a143342a60
mmman2000 <mmman2000@users.noreply.github.com>
2026-07-03 21:20:49 +0800

    Remove ad script, update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 11234691
+    "zoneId": 11234764
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
